99b0b8c53fce316b62477f12d4dd53df923ff913
Karthik <karthikkarkera86@gmail.com>
2021-12-27 01:23:08 +0530

    first

---
+++ components/Center.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { recipe_api } from "../lib/recipe";
+
+function Center() {
+  const [recipes, getRecipes] = useState([]);
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+    const response = await fetch(`${recipe_api}`);
+    const data = await response.json();
+    getRecipes(data.hits);
+  };
+  console.log(recipes);
+  return (
+    <div>
+      <div className="flex flex-wrap items-center justify-center">
+        {recipes.map((recipe) => (
+          <div className="border m-5 border-gray-200 rounded-lg max-w-lg">
+            <img
+              src={recipe.recipe.image}
+              className="border-t rounded-t w-full"
+            />
+            <div className="bg-gray-50 p-3">
+              <h2 className="text-sm md:text-lg font-semibold text-gray-700">
+                {recipe.recipe.label}
+              </h2>
+              <p className="capitalize text-sm text-gray-500">
+                {recipe.recipe.cuisineType} | {recipe.recipe.dishType}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Center;
